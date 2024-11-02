6f28b970eff28fa1ac02340139141c33877a8e88
HosseinSayyedMousavi <hossein.sayyedmousavi@partdp.ir>
2024-11-02 15:22:44 +0330

    action model and it's migration files

--- sport/models.py
+++ sport/models.py
@@ -1,4 +1,14 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 # Create your models here.
+
+class Action(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(null=True,blank=True)
+    
+    created_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
 
 
