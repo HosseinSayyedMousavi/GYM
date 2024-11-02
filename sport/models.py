d022afa749ce97a4a88234b57ec65599bb36efe6
HosseinSayyedMousavi <hossein.sayyedmousavi@partdp.ir>
2024-11-02 15:44:03 +0330

    git commit -m "plan model and it's migration files"

--- sport/models.py
+++ sport/models.py
@@ -22,7 +22,25 @@ class Diet(models.Model):
     course = models.ForeignKey(Course,on_delete=models.CASCADE)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
-
     description = models.TextField()
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+class Plan(models.Model):
+    course = models.ForeignKey(Course,on_delete=models.CASCADE)
+    action = models.ForeignKey(Action,on_delete=models.CASCADE)
+    set_number = models.PositiveIntegerField()
+    number_per_set = models.PositiveIntegerField()
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    day_of_week = models.CharField(max_length=10 , choices=(('saturday','saturday'),
+                                                                                ('sunday','sunday'),
+                                                                                ('monday','monday'),
+                                                                                ('tuesday','tuesday'),
+                                                                                ('wednesday','wednesday'),
+                                                                                ('thursday','thursday'),
+                                                                                ('friday','friday')))
+
     created_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
