a8bc82303b127339ebb79a837238fc9935bb93f2
maryam.rasoolizadeh <maryrasooli3@gmail.com>
2024-11-09 00:17:56 +0330

    plan urls added

--- sport/api/v1/urls.py
+++ sport/api/v1/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('action/',views.ActionListAPIView.as_view(),name='actions'),
     path('diet/',views.DietAPIView.as_view(),name='diets'),
     path('diet/<int:id>/',views.UpdateDietAPIView.as_view(),name='update-diets'),
+    path('plan/',views.PlanAPIView.as_view(),name='plans'),
+    path('plan/<int:id>/',views.UpdatePlanAPIView.as_view(),name='update-plans'),
     ]
