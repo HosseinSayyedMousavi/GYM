2e97ae058395759f47c014cace6c847f8adfba97
HosseinSayyedMousavi <hossein.sayyedmousavi@gmail.com>
2024-11-10 15:32:58 +0330

    Edit queryset of ConversationAPIView and ConversationAPIView

--- messenger/serializers.py
+++ messenger/serializers.py
@@ -1,16 +1,14 @@
 from rest_framework import serializers ,exceptions 
 from .models import Conversation , ConversationMessage
 from django.utils.translation import gettext as _
-from django.core.validators import MaxLengthValidator
 from django.core.paginator import Paginator
-import difflib
+
 
 class CreateConversationSerializer(serializers.ModelSerializer):
-    message = serializers.CharField(required=True,validators=[MaxLengthValidator(1000)])
+    message = serializers.CharField(required=True,max_length=1000)
     class Meta:
         model = Conversation
         fields = ("sender","receiver","title","message")
-
 
 class ConversationSerializer(serializers.ModelSerializer):
     class Meta: 
